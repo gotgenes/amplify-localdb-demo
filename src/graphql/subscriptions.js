e0ce7316bf493cbe0f9cf08804dca0738f1fdfe7
Chris Lasher <chris.lasher@gmail.com>
2020-12-03 21:11:33 -0800

    Add GraphQL API.

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMessage = /* GraphQL */ `
+  subscription OnCreateMessage {
+    onCreateMessage {
+      id
+      content
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateMessage = /* GraphQL */ `
+  subscription OnUpdateMessage {
+    onUpdateMessage {
+      id
+      content
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteMessage = /* GraphQL */ `
+  subscription OnDeleteMessage {
+    onDeleteMessage {
+      id
+      content
+      createdAt
+      updatedAt
+    }
+  }
+`;
