6219d0e0497fe298f9a27ba4fa75f3c599d8e237
Chris Lasher <chris.lasher@gmail.com>
2021-02-03 19:45:28 -0800

    Add lambda that inserts a new message.

---
+++ amplify/backend/function/addMessage/src/index.js
@@ -0,0 +1,33 @@
+/* Amplify Params - DO NOT EDIT
+	API_LOCALDB_GRAPHQLAPIIDOUTPUT
+	API_LOCALDB_MESSAGETABLE_ARN
+	API_LOCALDB_MESSAGETABLE_NAME
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const params = {
+  TableName: process.env.API_LOCALDB_MESSAGETABLE_NAME,
+  Item: {
+    id: uuid.v4(),
+    message: 'This is the message'
+  }
+};
+
+async function createItem() {
+  await docClient.put(params).promise();
+}
+
+exports.handler = async (event) => {
+  try {
+    await createItem();
+    return { body: 'Successfully created item!' };
+  } catch (err) {
+    return { error: err };
+  }
+};
